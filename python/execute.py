49e1ee9038fa290f87f6d252d603f9037c4bea15
Nils Söderman <rymdnisse@live.com>
2022-09-26 07:46:57 +0200

    Converted the execute.py module to use snake_case for functions/variables to follow the Unreal standard

--- python/execute.py
+++ python/execute.py
@@ -2,16 +2,14 @@
 Module for executing python code in Unreal
 """
 
-# pylint: disable-all
-
 import contextlib
-from genericpath import isfile
 import traceback
 import tempfile
 import json
 import sys
 import os
 import re
+
 
 TEMP_FOLDERPATH = os.path.join(tempfile.gettempdir(), "VSCode-Unreal-Python")
 OUTPUT_FILENAME = "exec-out"
@@ -21,14 +19,14 @@ DATA_FILEPATH_GLOBAL_VAR_NAME = "data_filepath"
 
 def read_input_data():
     """ Read input data that was written with typescript (`writeDataFile()` in `execute-script.ts`) """
-    Filepath = os.path.join(TEMP_FOLDERPATH, globals().get(DATA_FILEPATH_GLOBAL_VAR_NAME))
-    if os.path.isfile(Filepath):
-        with open(Filepath, 'r') as VsCodeSettingsFile:
-            return json.load(VsCodeSettingsFile)
+    filepath = os.path.join(TEMP_FOLDERPATH, globals().get(DATA_FILEPATH_GLOBAL_VAR_NAME))
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as vs_code_settings_file:
+            return json.load(vs_code_settings_file)
     return {}
 
 
-def GetExecGlobals():
+def get_exec_globals():
     """ Get globals to be used in the exec function when executing user scripts """
     if "__VsCodeVariables__" not in globals():
         globals()["__VsCodeVariables__"] = {
@@ -36,63 +34,63 @@ def GetExecGlobals():
     return globals()["__VsCodeVariables__"]
 
 
-def VsCodeExecuteCode(Code, Filename, bVsCodeIsDebugging):
+def execute_code(code, filename, is_vscode_debugging):
     try:
-        exec(compile(Code, Filename, "exec"), GetExecGlobals())
+        exec(compile(code, filename, "exec"), get_exec_globals())
     except Exception as e:
-        ExceptionType, Exc, Traceback = sys.exc_info()
+        exception_type, exc, traceback_type = sys.exc_info()
 
-        TracebackLines = []
-        for Line in traceback.format_exception(ExceptionType, Exc, Traceback):
-            if VsCodeExecuteCode.__name__ in Line:
+        traceback_lines = []
+        for line in traceback.format_exception(exception_type, exc, traceback_type):
+            if execute_code.__name__ in line:
                 continue
 
             # Reformat path to include the file number, example: 'myfile.py:5'
-            if re.findall('file ".*", line \d*, in ', Line.lower()):
-                Components = Line.split(",", 2)
-                LineNumber = "".join(x for x in Components[1] if x.isdigit())
-                Components[0] = '%s:%s"' % (Components[0][:-1], LineNumber)
-                Line = ",".join(Components)
-            Line = Line.replace('"', "")
+            if re.findall(r'file ".*", line \d*, in ', line.lower()):
+                components = line.split(",", 2)
+                line_number = "".join(x for x in components[1] if x.isdigit())
+                components[0] = f'"{components[0][:-1]}:{line_number}"'
+                line = ",".join(components)
+            line = line.replace('"', "", 1)
 
-            TracebackLines.append(Line)
+            traceback_lines.append(line)
 
-        TracebackMessage = "".join(TracebackLines).strip()
+        traceback_message = "".join(traceback_lines).strip()
         # Color the message red (this is only supported by 'Debug Console' in VsCode, and not not 'Output' log)
-        if bVsCodeIsDebugging:
-            TracebackMessage = '\033[91m' + TracebackMessage + '\033[0m'
+        if is_vscode_debugging:
+            traceback_message = '\033[91m' + traceback_message + '\033[0m'
 
-        print(TracebackMessage)
+        print(traceback_message)
 
 
 def main():
-    VsCodeData = read_input_data()
-    bVsCodeDebugging = VsCodeData.get("is_debugging", False)
-    AdditionalPrintStr = VsCodeData.get("additionalPrint", "")
+    vscode_data = read_input_data()
+    is_vscode_debugging = vscode_data.get("is_debugging", False)
+    additional_print_str = vscode_data.get("additionalPrint", "")
 
     # Set some global variables
-    ExecGlobals = GetExecGlobals()
+    exec_globals = get_exec_globals()
 
-    TargetFilepath = VsCodeData.get("__file__", "")
-    ExecGlobals["__file__"] = TargetFilepath
-    if "__name__" in VsCodeData and VsCodeData["__name__"]:
-        ExecGlobals["__name__"] = VsCodeData["__name__"]
-    elif "__name__" in ExecGlobals:
-        ExecGlobals.pop("__name__")
-        
-    command_id = VsCodeData.get("id", "")
-    OutputFilepath = os.path.join(TEMP_FOLDERPATH, f"{OUTPUT_FILENAME}-{command_id}.txt")
-    
-    with open(VsCodeData["file"], 'r') as VsCodeInFile:
-        if not bVsCodeDebugging and sys.version_info.major >= 3:
+    target_filepath = vscode_data.get("__file__", "")
+    exec_globals["__file__"] = target_filepath
+    if "__name__" in vscode_data and vscode_data["__name__"]:
+        exec_globals["__name__"] = vscode_data["__name__"]
+    elif "__name__" in exec_globals:
+        exec_globals.pop("__name__")
+
+    command_id = vscode_data.get("id", "")
+    output_filepath = os.path.join(TEMP_FOLDERPATH, f"{OUTPUT_FILENAME}-{command_id}.txt")
+
+    with open(vscode_data["file"], 'r') as vscode_in_file:
+        if not is_vscode_debugging and sys.version_info.major >= 3:
             # Re-direct the output through a text file
-            with open(OutputFilepath, 'w') as VsCodeOutFile, contextlib.redirect_stdout(VsCodeOutFile):
-                VsCodeExecuteCode(VsCodeInFile.read(),
-                                  TargetFilepath, bVsCodeDebugging)
+            with open(output_filepath, 'w') as vscode_out_file, contextlib.redirect_stdout(vscode_out_file):
+                execute_code(vscode_in_file.read(),
+                                  target_filepath, is_vscode_debugging)
         else:
-            VsCodeExecuteCode(VsCodeInFile.read(), TargetFilepath, bVsCodeDebugging)
+            execute_code(vscode_in_file.read(), target_filepath, is_vscode_debugging)
 
-        if AdditionalPrintStr:
-            print(AdditionalPrintStr)
-            
+        if additional_print_str:
+            print(additional_print_str)
+
 main()
