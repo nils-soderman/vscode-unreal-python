4204a24c5c71764c73121f8fe68256c211f37c27
Nils Söderman <rymdnisse@live.com>
2022-10-25 08:48:31 +0200

    All items are now added to the sidebar

--- resources/webviews/sidepanel/main.js
+++ resources/webviews/sidepanel/main.js
@@ -58,13 +58,14 @@ function onDocumentationShown() {
 //                             Functions
 // ---------------------------------------------------------------------
 
+const classSubItemTypes = ["Method", "Class Method", "Property", "Constant"];
+
+
 class DocumentationPage {
 
     rawTableOfContents = null;
     content = [];
     titles = [];
-
-    body;
 
     /**
      * 
@@ -72,25 +73,36 @@ class DocumentationPage {
      */
     constructor(tableOfContents) {
         this.rawTableOfContents = tableOfContents;
-
-        this.body = window.document.getElementById("content-body");
-
+    
         this._buildPage();
     }
-
+    
     _buildPage() {
         const builder = new DocumentationPageBuilder();
-
+        
+        const body = window.document.getElementById("content-body");
+        
         for (const [key, value] of Object.entries(this.rawTableOfContents)) {
-            const title = builder.createTitle(key);
-            this.body.appendChild(title);
+            const section = builder.createSection(key);
             
             if (Array.isArray(value)) {
                 // Functions, since they are a flat list
+                value.forEach(name => {
+                    builder.addMainItem(name);
+                });
             }
             else {
-
+                for (const [name, data] of Object.entries(value)) {
+                    builder.addMainItem(name);
+                    classSubItemTypes.forEach(subType => {
+                        data[subType].forEach(subName => {
+                            builder.addSubItem(subName, subType);
+                        });
+                    });
+                }
             }
+
+            body.appendChild(section);
         }
     }
 
@@ -104,11 +116,76 @@ class DocumentationPage {
 }
 
 class DocumentationPageBuilder {
+    currentItemContainer;
+    currentItemName = "";
+
+    /**
+     * 
+     * @param {string} text 
+     * @returns {HTMLHeadingElement}
+     */
     createTitle(text) {
         const header = document.createElement('h2');
         header.append(text);
         return header;
     }
+
+    /**
+     * 
+     * @param {string} title 
+     * @returns {HTMLDivElement}
+     */
+    createSection(title) {
+        const titleElement = this.createTitle(title);
+
+        const itemContainer = document.createElement('div');
+        itemContainer.id = `${title}-items`;
+
+        this.currentItemContainer = itemContainer;
+
+        const baseElelemt = document.createElement('div');
+        baseElelemt.id = title;
+
+        baseElelemt.appendChild(titleElement);
+        baseElelemt.appendChild(itemContainer);
+
+        return baseElelemt;
+    }
+
+    addMainItem(name) {
+        if (!this.currentItemContainer) {
+            throw new Error("currentItemContainer has not been set!");
+        }
+
+        const item = document.createElement('div');
+        item.append(name);
+        
+        item.className = "doc-item";
+        
+        this.currentItemName = name;
+        this.currentItemContainer.appendChild(item);
+        
+        return item;
+    }
+
+    addSubItem(name, type) {
+        if (!this.currentItemContainer) {
+            throw new Error("currentItemContainer has not been set!");
+        }
+
+        const subItem = document.createElement('div');
+        subItem.append(`${this.currentItemName}.${name} [${type}]`);
+
+        subItem.className = "doc-item-sub";
+
+        subItem.hidden = true;
+
+        this.currentItemContainer.appendChild(subItem);
+
+        return subItem;
+    }
+
+
 }
 
 
