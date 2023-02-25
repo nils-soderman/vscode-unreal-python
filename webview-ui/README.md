54b8162e005a32e0c4bbab956f5d99a089613799
Nils Söderman <rymdnisse@live.com>
2023-02-25 18:09:52 +0100

    Initial setup of a webview using react

---
+++ webview-ui/README.md
@@ -0,0 +1,5 @@
+# Sidebar UI
+
+This folder contains the front-end of the webview sidebar provided by this extension
+
+The sidebar is written in [React](https://reactjs.org)
---
+++ webview-ui/src/Documentation/Index/docIndex.jsx
@@ -0,0 +1,12 @@
+// Setup a basic react component module
+
+import React, { Component } from 'react';
+
+export default class DocIndex extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>Documentation</h1>
+        );
+    }
+}
