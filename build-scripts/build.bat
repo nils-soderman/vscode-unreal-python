bbc433d8cb46ce32994ada2fac7449b2b1f590a0
Nils Söderman <rymdnisse@live.com>
2023-03-12 19:13:25 +0100

    build the webview

--- build-scripts/build.bat
+++ build-scripts/build.bat
@@ -1,10 +1,16 @@
+@REM Build the webview
+cd "%~dp0../webview-ui"
+call build.bat
+
+@REM Clear extension output
 cd "%~dp0../"
 rmdir /S /Q "./out/"
 
+@REM Make sure the output directory exists
 set OutDir="./builds/"
-
 if not exist %OutDir% (
     mkdir %OutDir%
 )
 
+@REM Build the extension
 vsce package --out %OutDir%
