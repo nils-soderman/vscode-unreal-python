148897f3bb8214ad4470e85e2f96187c49182d74
Nils Söderman <rymdnisse@live.com>
2023-03-16 08:54:19 +0100

    Autofocus the searchbar

--- webview-ui/src/Documentation/Index/Header/docHeader.tsx
+++ webview-ui/src/Documentation/Index/Header/docHeader.tsx
@@ -1,6 +1,6 @@
 import "./docHeader.scss"
 
-import { Component } from 'react';
+import { Component, createRef } from 'react';
 import { VSCodeTextField } from '@vscode/webview-ui-toolkit/react';
 
 interface Props {
@@ -13,12 +13,26 @@ interface State {
 
 class DocHeader extends Component<Props, State> {
     state = {}
+    
+    textField = createRef<HTMLDivElement>();
+
+    componentDidUpdate() {
+        // TODO: Because 'autofocus' is currently broken w/ webview-ui-toolkit/react
+        // Swap to using autofocus when this is fixed: https://github.com/microsoft/vscode-webview-ui-toolkit/issues/381
+        const shadowRoot = this.textField.current.shadowRoot;
+        if (shadowRoot) {
+            const input = shadowRoot.querySelector('input');
+            if (input) {
+                input.focus();
+            }
+        }
+    }
 
     render() {
         return (
             <div className="doc-index-header">
                 {/* Callback OnInput: typehint variable e */}
-                <VSCodeTextField id='searchbar' placeholder='Search documentation...' onInput={(e) => { if (this.props.handleSearchInput) this.props.handleSearchInput(e.target.value) }} />
+                <VSCodeTextField autofocus id='searchbar' placeholder='Search documentation...' onInput={(e) => { if (this.props.handleSearchInput) this.props.handleSearchInput(e.target.value) }} ref={this.textField}/>
             </div>
         );
     }
