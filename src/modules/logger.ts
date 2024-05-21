950c3eb66c70df706cfbd9e3a24170fa508642b6
Nils Söderman <rymdnisse@live.com>
2024-05-21 20:51:16 +0200

    Added function `logError`

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -10,10 +10,34 @@ function getOutputChannel() {
 }
 
 
+/**
+ * Log a message to the output channel
+ * @param message The message to log
+ */
 export function log(message: string) {
     getOutputChannel().appendLine(message);
 }
 
+
+/**
+ * Show the output channel to the user
+ */
 export function show() {
     getOutputChannel().show(true);
+}
+
+
+/**
+ * Show an error message to the user and log the full error message
+ * @param message The message to show to the user in the error dialog
+ * @param fullLog The full error message to log
+ */
+export function logError(message: string, fullLog: Error) {
+    log(fullLog.toString());
+
+    vscode.window.showErrorMessage(message, "Show Log").then((value) => {
+        if (value === "Show Log") {
+            show();
+        }
+    });
 }
